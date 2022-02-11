5d6173be05f5cf9864025579a9376f346f2de721
huettemann <nils_huettemann@mail.de>
2022-02-11 16:36:26 +0100

    little change on init.lua ultisnip

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -63,9 +63,9 @@ end)
     },
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
-      { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'vsnip' }, -- For vsnip users.
       -- { name = 'luasnip' }, -- For luasnip users.
-      -- { name = 'ultisnips' }, -- For ultisnips users.
+      { name = 'ultisnips' }, -- For ultisnips users.
       -- { name = 'snippy' }, -- For snippy users.
     }, {
       { name = 'buffer' },
