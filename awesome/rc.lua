1ca5e9f1e7e4b7eaf3c31f844f37b0be63045b5a
huettemann <nils_huettemann@mail.de>
2022-08-14 17:00:50 +0200

    added autorun seaf-cli start

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -619,14 +619,16 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 -- gaps
 -- beautiful.useless_gap = 3
 
--- Autorun programs
+-- Autorun autostart programs
 autorun = true
 autorunApps =
 {
    "picom",
    "nextcloud",
    "keepassxc",
-   "nm-applet"
+   "nm-applet",
+   "seaf-cli start"
+
    -- "polkit-gnome-authentification-agent-1",
    -- "nm-applet",
    -- "xapp-sn-watcher",
